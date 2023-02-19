4239276ffcfc1c26c8acfb4a59bab60aecbdf14c
Martin <42579919+mebarron@users.noreply.github.com>
2023-02-18 23:41:08 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ export TF_VAR_AWS_ACCOUNT_A_NUMBER=
 export TF_VAR_AWS_ACCOUNT_B_NUMBER=
 export TF_VAR_AWS_DX_ACCOUNT_NUMBER=
 
-python3 tfrunner.py account_a account_b account_a_attachment account_b_attachment dxgw_account apply
+python3 tfrunner.py account_a account_b account_a_attachment account_b_attachment direct_connect apply
