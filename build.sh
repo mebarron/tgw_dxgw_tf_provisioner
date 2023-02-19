eff45cc9a429355059eb8d0fade2a8be10edb4f1
Martin Barron <mebarron@me.com>
2023-02-19 02:15:54 -0500

    Update build and destroy scripts

--- build.sh
+++ build.sh
@@ -4,8 +4,4 @@ export TF_VAR_AWS_ACCOUNT_A_NUMBER=
 export TF_VAR_AWS_ACCOUNT_B_NUMBER=
 export TF_VAR_AWS_DX_ACCOUNT_NUMBER=
 
-<<<<<<< HEAD
 python3 tfexecutor.py account_a account_b account_a_attachment account_b_attachment dxgw_account apply
-=======
-python3 tfrunner.py account_a account_b account_a_attachment account_b_attachment direct_connect apply
->>>>>>> refs/remotes/origin/main
